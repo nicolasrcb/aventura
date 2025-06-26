eb7a0b6943ae74d163af63c572e5377a5354de71
nicolasrcb <nicolas.cortes.batista@escola.pr.gov.br>
2025-06-26 11:42:38 +0000

    26 do 6

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const avanca = document.querySelector('.btn-proximo');
 
 avanca.array.forEach(button => {
     button.addEventListener('click', function (){
+       const atual = document.querySelector('.ativo');
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
+       atual.classList.remove('ativo');
+       document.getElementById(proximoPasso).classList.add('ativo');
     })
-     
 })
